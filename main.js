c613cd8374a1219282512cc87cf704a84e316d1d
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-02-24 17:38:53 -0300

    Alteração para o evento de tecla onkeydown

--- main.js
+++ main.js
@@ -10,11 +10,19 @@ for (let contador = 0; contador < listaDeTeclas.length; contador += 1 ) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
-
     const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-}
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.remove('ativa');
+    }
+    
+
+} 
