631a1f60872659d581fda4d378450477554550a2
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-02-24 14:45:04 -0300

    Melhoria de código usando for

--- main.js
+++ main.js
@@ -5,20 +5,16 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador += 1 ) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
 
-    const idAudio = `#som_${instrumento}`;
+
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador += 1;
-
-  
 }
